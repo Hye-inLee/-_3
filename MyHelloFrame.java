265fa99f0fbb61329e0cccb3a351eadc81ffcc11
choisewon <chltpdnjs5@naver.com>
2019-11-11 13:14:01 +0900

    MyFrame를 작성하였습니다.

---
+++ MyHelloFrame.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ * 여기에 MyHelloFrame 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class MyHelloFrame extends JFrame
+{
+    public JLabel ML;
+    public MyHelloFrame()
+    {
+        
+        this.setTitle("실습_3(2019.11.11)");
+        this.setSize(300,400);
+        
+        //ML = new MyHelloPanelListener();
+        this.add(ML);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.setVisible(true);
+    }
+}
